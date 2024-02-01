b4a0c75d98720f7ce074258e3a7e5a46d7130939
Sugakesshaa <sugakesshaa@pixelos.net>
2024-02-02 01:13:00 +0800

    touchscreen: fts_spi/goodix_g9916: Add gpio check for driver init
    - used by diting to load the correct touchpanel driver on init
    
    Change-Id: Ief207a81fc7936f4e341104cdd402b80692a155e

--- drivers/input/touchscreen/fts_spi/fts.c
+++ drivers/input/touchscreen/fts_spi/fts.c
@@ -67,6 +67,8 @@
 #include <linux/uaccess.h>
 #include <linux/debugfs.h>
 
+#define L12_ID_DET (301+119)
+
 #ifdef KERNEL_ABOVE_2_6_38
 #include <linux/input/mt.h>
 #endif
@@ -9456,6 +9458,16 @@ static struct spi_driver fts_spi_driver = {
 
 static int __init fts_driver_init(void)
 {
+	int gpio_119;
+	gpio_direction_input(L12_ID_DET);
+	gpio_119 = gpio_get_value(L12_ID_DET);
+	logError(1, "%s gpio_119 = %d\n", tag, gpio_119);
+	if(!gpio_119){
+		logError(1,"%s TP is goodix\n",tag);
+		return 0;
+	}else{
+		logError(1,"%s TP is st 61y\n",tag);
+	}
 #ifdef I2C_INTERFACE
 	return i2c_add_driver(&fts_i2c_driver);
 #else
